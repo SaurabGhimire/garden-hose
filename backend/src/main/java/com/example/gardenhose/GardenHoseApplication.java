59a10d079bbf0f6f47cd2fad1b6ec8e2fbf5cb10
Saurab Ghimire <sourrab@gmail.com>
2025-08-02 17:32:21 -0400

    backend and frontend project structure integrated

---
+++ backend/src/main/java/com/example/gardenhose/GardenHoseApplication.java
@@ -0,0 +1,13 @@
+package com.example.gardenhose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenHoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenHoseApplication.class, args);
+	}
+
+}
